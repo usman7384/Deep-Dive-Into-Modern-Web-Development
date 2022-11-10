b2b2572329ce6327438d7409f2c80759c588a9d7
usman7384 <mu3931368@gmail.com>
2022-11-10 22:27:51 +0500

    revert solved

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,83 +1,70 @@
-import React, { useState } from 'react';
-import Course from "./Course" 
+import React from 'react';
 
 const App = () => {
+
   const course = {
-    name: "Half Stack application development",
+    name: 'Half Stack application development',
     parts: [
       {
-        name: "Fundamentals of React",
+        name: 'Fundamentals of React',
         exercises: 10
       },
       {
-        name: "Using props to pass data",
+        name: 'Using props to pass data',
         exercises: 7
-      }
-      ,
+      },
       {
-        name: "State of a component",
+        name: 'State of a component',
         exercises: 14
       }
     ]
-  };
-  
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
-  
-  
+  }
+
+ 
+
   return (
     <div>
-    {courses.map((course)=>(
-      <Course key={course.id} course={course.name} parts={course.parts} />
-      ))}
-      </div>
-      );
-    };
-    
-    
-    
-    export default App;
-    
-    
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+     <Part name={props.parts[0].name} exercises={props.parts[0].exercises}/>
+     <Part name={props.parts[1].name} exercises={props.parts[1].exercises}/>
+     <Part name={props.parts[2].name} exercises={props.parts[2].exercises}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    </div>
+  )
+}
+
+const Part = (props) =>{
+  return (
+    <p>
+    {props.name} {props.exercises}
+    </p>
+  
+  )
+}
+
+export default App
---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,112 @@
+import { useState } from "react";
+
+const App = () => {
+
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [showAll, setShowAll] = useState([{}]);
+
+
+  
+  const addPerson = (event) => {
+    event.preventDefault();
+    if (persons.find((person) => person.name === newName)) {
+      window.alert(`${newName} is already added to phonebook`);
+      return false;
+    }
+    const newObj = {
+      name: newName,
+      number: newNumber,
+    };
+    setPersons(persons.concat(newObj));
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+  const handleNumberChange = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
+
+  const searchPerson = (event) => {
+    console.log("sisisi")
+    const persontoSearch = event.target.value;
+    var copyPersons = [...persons];
+    copyPersons = copyPersons.filter(
+      (item) =>
+        item.name.toLowerCase().indexOf(persontoSearch.toLowerCase()) !== -1
+    );
+    setShowAll(copyPersons);
+  };
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter array={showAll} searchPerson={searchPerson} />
+      {/* <h2>add new</h2>
+      <PersonForm newName={newName} newNumber={newNumber} addPerson={addPerson()} handleNameChange={handleNameChange()} handleNumberChange={handleNumberChange()}/> */}
+      <h2>Numbers</h2>
+      <Person array={persons}/>
+    </div>
+  );
+};
+
+const Person = (props) => {
+return(
+  <div>
+    {props.array.map((person, key) => (
+      <div key={key}>
+        {person.name} {person.number}
+      </div>
+    ))}
+  </div>)
+};
+
+const Filter = (props) => {
+  return (
+    <div>
+      <form>
+        <div>
+
+          filter shown with <input onChange={props.searchPerson} />
+        </div>
+      </form>
+      <div>
+        <div>
+          {props.array.map((person, key) => (
+            <div key={key}>
+              {person.name} {person.number}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const PersonForm = (addPerson,handleNameChange,handleNumberChange,newName,newNumber) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+export default App;
