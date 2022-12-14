554f72eff0563e991474a0237c6429ba6d3cae5e
usman7384 <mu3931368@gmail.com>
2022-12-14 16:30:01 +0500

    3.15-3.18

--- part3/phonebookDatabase/index.js
+++ part3/phonebookDatabase/index.js
@@ -11,44 +11,25 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.static("build"));
 
-morgan.token(function (req, res) {
-  return `${JSON.stringify(req.body)}`;
-});
-app.use(
-  morgan(
-    ":method :url :status :res[content-length] :response-time :req[header]"
-  )
-);
+morgan.token('ob', function (req, res) { 
+  console.log("ob", req.body)
+  return `${JSON.stringify(req.body)}` })
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+app.use(morgan(':method :url :status :response-time :req[header] :ob'))
 
-morgan.token("ob", function (req, res) {
-  console.log("ob", req.body);
-  return `${JSON.stringify(req.body)}`;
-});
 
-app.use(morgan(":method :url :status :response-time :req[header] :ob"));
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((result) => {
@@ -58,27 +39,35 @@ app.get("/api/persons", (request, response) => {
 
 app.get("/info", (request, response) => {
   const timestamp = new Date(Date.now());
-  response.send(
-    `<p> Phonebook has info for ${
-      persons.length
-    } people </p> <p> ${timestamp.toUTCString()}</p>`
-  );
+  var countInfo = Person.find();
+  countInfo.count(function (err, count) {
+    if (err) console.log(err);
+    else {
+      response.send(
+        `<p> Phonebook has info for ${count} people </p> <p> ${timestamp.toUTCString()}</p>`
+      );
+    }
+  });
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
-  const person = persons.find((person) => person.id == id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  Person.findById(id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id != id);
-  response.status(204).end();
+  const id = request.params.id;
+  Person.findByIdAndRemove(id).then((result) => {
+    response.status(204).end();
+  });
 });
 
 app.post("/api/persons", (request, response) => {
@@ -111,6 +100,19 @@ app.post("/api/persons", (request, response) => {
       response.json(newPerson);
     });
   });
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+  const person = {
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 const PORT = process.env.PORT || 3001;
