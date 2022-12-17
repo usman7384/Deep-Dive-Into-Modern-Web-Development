af0ec36ccd1f6e89fd35efa221532e8fef583448
usman7384 <mu3931368@gmail.com>
2022-12-18 02:57:25 +0500

    4.15

--- part4/blogList/app.js
+++ part4/blogList/app.js
@@ -2,10 +2,11 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const Router = require('./controllers/blog')
+const blogRouter = require('./controllers/blog')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const usersRouter = require('./controllers/user')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -22,8 +23,8 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.use('/api/blogs', Router)
-
+app.use('/api/blogs', blogRouter)
+app.use('/api/users', usersRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
--- part4/blogList/controllers/blog.js
+++ part4/blogList/controllers/blog.js
@@ -7,12 +7,55 @@ Router.get("/", (request, response) => {
   });
 });
 
+Router.get("/:id", (request, response) => {
+  Blog.findById(request.params.id)
+    .then((blog) => {
+      if (blog) {
+        response.json(blog);
+        response.status(200).end();
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
 Router.post("/", (request, response) => {
   const blog = new Blog(request.body);
+  if (typeof blog.likes === "undefined" || blog.likes === null) {
+    blog.likes = 0;
+  }
+  if (
+    typeof blog.title === "undefined" ||
+    blog.title === null ||
+    typeof blog.url === "undefined" ||
+    blog.url === null
+  ) {
+    response.status(400).end();
+  } else {
+    blog.save().then((result) => {
+      response.status(201).json(result);
+    });
+  }
+});
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
+Router.delete("/:id", (request, response) => {
+  const id = request.params.id;
+  Blog.findByIdAndRemove(id).then((result) => {
+    response.status(204).end();
   });
 });
 
-module.exports = Router
+Router.put("/:id", (request, response, next) => {
+  const body = request.body;
+  const blog = {
+    likes: body.likes,
+  };
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then((updatedBlog) => {
+      response.json(updatedBlog);
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = Router;
